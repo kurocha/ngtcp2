0b78b2bd94ba21cb1ebcf38e45bf04df6f94529d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-04-04 20:06:30 +1200

    Build with picotls.

--- teapot.rb
+++ teapot.rb
@@ -7,7 +7,7 @@ teapot_version "3.0"
 define_target "ngtcp2" do |target|
 	target.depends :platform
 	
-	target.depends "Library/z", public: true
+	target.depends "Library/picotls", public: true
 	
 	target.depends "Build/Make"
 	target.depends "Build/CMake"
@@ -15,10 +15,25 @@ define_target "ngtcp2" do |target|
 	target.provides "Library/ngtcp2" do
 		source_files = target.package.path + "ngtcp2"
 		cache_prefix = environment[:build_prefix] / environment.checksum + "ngtcp2"
-		package_files = cache_prefix / "lib/libngtcp2.a"
+		package_files = [
+			cache_prefix / "lib/libngtcp2.a",
+			cache_prefix / "lib/libngtcp2_crypto_picotls.a",
+		]
+		
+		picotls_path = environment[:picotls_path]
+		
+		picotls_libraries = [
+			"-lssl", "-lcrypto",
+			environment[:picotls_prefix] / "lib/libpicotls-openssl.a",
+			environment[:picotls_prefix] / "lib/libpicotls-core.a",
+		]
 		
 		cmake source: source_files, install_prefix: cache_prefix, arguments: [
-			"-DBUILD_SHARED_LIBS=OFF",
+			"-DENABLE_SHARED_LIB=OFF",
+			"-DENABLE_OPENSSL=OFF",
+			"-DENABLE_PICOTLS=ON",
+			"-DPICOTLS_INCLUDE_DIR=#{environment[:picotls_prefix] / 'include'}",
+			"-DPICOTLS_LIBRARIES=#{picotls_libraries.join(';')}",
 		], package_files: package_files
 		
 		append linkflags package_files
@@ -33,4 +48,6 @@ define_configuration "ngtcp2" do |configuration|
 	
 	configuration.require "build-make"
 	configuration.require "build-cmake"
+	
+	configuration.require "picotls"
 end
